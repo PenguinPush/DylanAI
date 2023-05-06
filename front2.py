c228dfaeff5c0047d2a00e310081084b1f2c31ea
suoeh <dylanydai@gmail.com>
2023-05-06 16:05:18 -0400

    Update front2.py

--- front2.py
+++ front2.py
@@ -1,4 +1,5 @@
 import customtkinter as ctk
+<<<<<<< Updated upstream
 import tkinter as tk
 from pathlib import Path
 from tkinter import *
@@ -14,6 +15,22 @@ root.geometry("800x600")
 def login():
     print("Test")
 c=0  # amount of inputs given
+=======
+
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("green")
+
+color = {"white": "#FFEAEC"}
+color = {"blackk": "#3C3744"}
+
+root = ctk.CTk()
+root.geometry("800x600")
+
+def login():
+    print("Test")
+
+
+>>>>>>> Stashed changes
 frame = ctk.CTkFrame(master=root)
 frame.pack(pady=20, padx=20, fill="both", expand=True)
 
@@ -24,6 +41,7 @@ scroll = ctk.CTkScrollableFrame(master=frame,corner_radius=20, fg_color="#3C3744
 scroll.pack(pady=(20,0), padx=0)
 
 def add():
+<<<<<<< Updated upstream
     global c, filename
 
     def browseFiles():
@@ -61,3 +79,26 @@ add = ctk.CTkButton(root, fg_color="#3C3744", text="delete",font=("nexa bold",20
 add.pack(side=ctk.LEFT, padx=(20,40), pady=(10,30), fill=ctk.BOTH, expand=True)
 
 root.mainloop()
+=======
+
+    x=ctk.CTkEntry(scroll,fg_color="#817A90",placeholder_text="Description",placeholder_text_color="#FFEAEC",font=("nexa bold", 20),border_width=0)
+    x.pack(side=ctk.RIGHT,pady=10, padx=(10,20),fill=ctk.BOTH, expand=True)
+
+    y=ctk.CTkEntry(scroll,fg_color="#817A90",placeholder_text="Name",placeholder_text_color="#FFEAEC",font=("nexa bold", 20),border_width=0)
+    y.pack(pady=10, padx=10,fill=ctk.BOTH, expand=True)
+
+    options = ["macro","website","open"]
+    z = ctk.CTkComboBox(scroll, values=options,fg_color="#817A90",dropdown_text_color="#FFEAEC",font=("nexa bold", 20),
+                            dropdown_fg_color="#817A90",border_width=0,text_color="#FFEAEC",dropdown_font=("nexa light", 20),
+                            button_color="#817A90",dropdown_hover_color="#3C3744",border_color="#817A90",state="readonly")
+    z.set(options[0])
+    z.bind("<<ComboboxSelected>>")
+    z.pack(padx=10, pady=10)
+
+add()
+
+add = ctk.CTkButton(root, fg_color="#3C3744", text="add",font=("nexa bold",20),text_color="#FFEAEC")
+add.pack(side=ctk.LEFT, padx=200, pady=(10,30), fill=ctk.BOTH, expand=True)
+
+root.mainloop()
+>>>>>>> Stashed changes
