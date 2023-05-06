bfcad3c4948a9c51043640d44cc297052c09c264
PenguinPush <icyzminecraft@gmail.com>
2023-05-06 02:06:17 -0400

    dsdads

--- transcript.py
+++ transcript.py
@@ -1,15 +1,9 @@
-import io
-
-import pyaudio
-import speech_recognition
-from pydub import AudioSegment
 import speech_recognition as sr
 import whisper
 import queue
 import tempfile
 import os
 import threading
-import click
 import torch
 import numpy as np
 import openai
@@ -24,9 +18,6 @@ class Variables:
     wav_checked = False
 
 def main(model, english,verbose, energy, pause,dynamic_energy,save_file,device):
-    #there are no english models for large
-    if model != "large" and english:
-        model = model + ".en"
     audio_model = whisper.load_model(model).to(device)
     audio_queue = queue.Queue()
     result_queue = queue.Queue()
@@ -38,9 +29,6 @@ def main(model, english,verbose, energy, pause,dynamic_energy,save_file,device):
     while True:
         text = result_queue.get()
         print(text)
-        transcript = open("transcript.txt", "a")
-        transcript.write(text + "\n")
-
 
 def record_audio(audio_queue, energy, pause, dynamic_energy, save_file, temp_dir):
     #load the speech recognizer and set the initial energy threshold and pause threshold
@@ -51,7 +39,6 @@ def record_audio(audio_queue, energy, pause, dynamic_energy, save_file, temp_dir
 
     with sr.Microphone(sample_rate=16000) as source:
         print("Model Loaded!")
-        i = 0
         while True:
             audio = r.listen(source, phrase_time_limit=5)
             data = np.frombuffer(audio.frame_data, np.int16).flatten().astype(np.float32) / 32768.0
@@ -59,6 +46,9 @@ def record_audio(audio_queue, energy, pause, dynamic_energy, save_file, temp_dir
             audio_data = Variables.filename
             write(Variables.filename, 16000, data)
             audio_queue.put_nowait(audio_data)
+
+            if os.path.exists(f"temp{Variables.i - 2}.wav"):
+                os.remove(f"temp{Variables.i - 2}.wav")
 
             Variables.i += 1
             Variables.wav_checked = False
@@ -83,8 +73,5 @@ if __name__ == "__main__":
     with sr.Microphone() as source:
         print("Please wait. Calibrating microphone...")
         r.adjust_for_ambient_noise(source, duration=5)
-
-    transcript = open("transcript.txt", 'r+')
-    transcript.truncate(0)
 
     main(model="base", device=("cuda" if torch.cuda.is_available() else "cpu"), english=True, verbose=False, energy=1000, dynamic_energy=True, pause=0.3, save_file=False)
