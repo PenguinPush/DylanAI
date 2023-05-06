aedb1f6b10a48ad7836e5a1638eae6e565febebf
Edward Ji <eveeorsmts@gmail.com>
2023-05-06 02:05:18 -0400

    eee

--- frontend.py
+++ frontend.py
@@ -1,3 +1,4 @@
+
 import customtkinter as ctk
 import tkinter as tk
 from tkinter import *
@@ -7,64 +8,63 @@ ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("green")
 
 # MENU START
-menuSwitch = False
-
-root = ctk.CTk()
-root.geometry("800x500")
-root.title("Dylan.Ai")
-
 color = {"nero": "#252726", "orange": "#FF8700", "darkorange": "#FE6101"}
 
-menuOpen = PhotoImage(file="menuOpen.png")
-menuClose = PhotoImage(file="menuClose.png")
+root = tk.Tk()
+root.title("Tkinter Navbar")
+root.config(bg="gray17")
+root.geometry("800x500")
 
+menuState = False
 
-def menuSwitchReal():
-    global menuSwitch
-    if menuSwitch is True:
+menuOpenImage = PhotoImage(file="menuOpen.png")
+menuCloseImage = PhotoImage(file="menuClose.png")
+
+def switch():
+    global menuState
+    if menuState is True:
         for x in range(301):
-            menuBar.place(x=-x, y=0)
-            root.update()
-        brandLabel.config(fg="green")
-        homeLabel.config(bg=color["orange"])
-        menuSwitch = False
-    else:
-        brandLabel.config(fg="#5F5A33")
-        homeLabel.config(bg=color["nero"])
-        for x in range(-300, 0):
-            menuBar.place(x=x, y=0)
-            root.update()
-        menuSwitch = True
+            menuFrame.place(x=-x, y=0)
+            barTop.update()
 
+       
+        barTop.config(bg=color["orange"])
+        root.config(bg="gray17")
+
+        menuState = False
+    else:
+       
+        barTop.config(bg=color["nero"])
+        root.config(bg=color["nero"])
+
+        for x in range(-300, 0):
+            menuFrame.place(x=x, y=0)
+            barTop.update()
+
+        menuState = True
 
 barTop = tk.Frame(root, bg=color["orange"], height=50)
 barTop.pack(side="top", fill=tk.X)
 
-homeLabel = tk.Label(barTop, text="PE", font="Bahnschrift 15", bg=color["orange"], fg="gray17", height=2, padx=20)
-homeLabel.pack(side="right")
-
-# Main label text:
-brandLabel = tk.Label(root, text="Pythonista\nEmpire", font="System 30", fg="green")
-brandLabel.place(x=100, y=250)
 
 
-menuOpenButton = tk.Button(barTop, image=menuOpen, bg=color["orange"], activebackground=color["orange"], bd=0, padx=20, command=menuSwitchReal)
-menuOpenButton.place(x=10, y=10)
+topBar = tk.Button(barTop, image=menuOpenImage, bg=color["orange"], activebackground=color["orange"], bd=0, padx=20, command=switch)
+topBar.place(x=10, y=10)
 
-menuBar = tk.Frame(root, bg="gray17", height=1000, width=300)
-menuBar.place(x=-300, y=0)
-tk.Label(menuBar, font="bahnschriftLight 15", bg=color["orange"], fg="black", height=2, width=300, padx=20).place(x=0, y=0)
+menuFrame = tk.Frame(root, bg="gray17", height=1000, width=300)
+menuFrame.place(x=-300, y=0)
+tk.Label(menuFrame, font="Bahnschrift 15", bg=color["orange"], fg="black", height=2, width=300, padx=20).place(x=0, y=0)
 
 y = 80
-
-options = ["Menu", "Macros", "Help"]
-
+options = ["Menu", "Dylan", "Help"]
 for i in range(3):
-    tk.Button(menuBar, text=options[i], font="bahnschriftLight 15", bg="gray17", fg=color["orange"], activebackground="gray17", activeforeground="green", bd=0).place(x=25, y=y)
+    tk.Button(menuFrame, text=options[i], font="BahnschriftLight 15", bg="gray17", fg=color["orange"], activebackground="gray17", activeforeground="green", bd=0).place(x=25, y=y)
     y += 40
 
-menuCloseButton = tk.Button(menuBar, image=menuClose, bg=color["orange"], activebackground=color["orange"], bd=0, command=menuSwitchReal)
-menuCloseButton.place(x=250, y=10)
+menuClose = tk.Button(menuFrame, image=menuCloseImage, bg=color["orange"], activebackground=color["orange"], bd=0, command=switch)
+menuClose.place(x=250, y=10)
+
+root.mainloop()
 
 
 # MENU CLOSE
