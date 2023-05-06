2447eba565bc648e22f4cfae6ee40e7ca079c677
suoeh <dylanydai@gmail.com>
2023-05-06 03:16:50 -0400

    dsa
    da

--- test.py
+++ test.py
@@ -1,15 +1,12 @@
-import customtkinter as ctk
+import ctk
 
-# Create the main window
-root = ctk.CTk()
+def validate_text(entry):
+    text = entry.get()
+    if len(text) > 10:
+        # If text is longer than 10 characters, truncate it to 10 characters
+        entry.set(text[:10])
+    return True
 
-# Create a label widget
-label1 = ctk.CTkLabel(root, text="Label 1")
-label1.pack()
-
-# Create a second label widget and place it below the first label
-label2 = ctk.CTkLabel(root, text="Label 2")
-label2.place(relx=0, rely=0, x=0, y=label1.winfo_height()+10)
-
-# Start the main event loop
-root.mainloop()
+# Create a CTKENTRY widget and add the validation function
+entry = ctk.CTKENTRY()
+entry.add_validator(validate_text)
