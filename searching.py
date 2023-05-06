422a0e4b9e5cff65a7d895d5be615b5501af9cda
Edward Wang <115112114+Edward-W7@users.noreply.github.com>
2023-05-06 09:16:34 -0400

    Update searching.py

--- searching.py
+++ searching.py
@@ -10,9 +10,9 @@ def google_search(search_term, api_key, cse_id, **kwargs):
     res = service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()
     return res['items']
 
-def search_results(search_term):
+def search_results(search_term, results_number=3):
     try:
-        results = google_search(search_term, my_api_key, my_cse_id, num=3)
+        results = google_search(search_term, my_api_key, my_cse_id, num=results_number)
     except KeyError:
         results = None
 
