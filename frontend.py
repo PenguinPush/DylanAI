aba9e7d2973d2b88ce6e03bc6d613af05e90daff
suoeh <dylanydai@gmail.com>
2023-05-05 20:47:39 -0400

    i fucked up
    hi

--- frontend.py
+++ frontend.py
@@ -1,18 +1,28 @@
-import tkinter as tk
-import customtkinter 
-from tkinter import *
-from tkinter import ttk
+import customtkinter as ctk
+
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("green")
+
+root = ctk.CTk()
+root.geometry("800x500")
+
+def login():
+    print("Test")
 
 
-# sidebar
+frame = ctk.CTkFrame(master=root)
+frame.pack(pady=20, padx=60, fill="both", expand=True)
 
-root = tk.Tk()
-root.title("Polar")
-root.geometry("750x450")
+label = ctk.CTkLabel(master=frame, text="POLAR", text_color="#FFEAEC", font=("nexa bold", 30))
+label.pack(pady=12, padx=10)
 
-State = False
+button1 = ctk.CTkLabel(master=frame, text="Login")
+button1.pack(pady=12, padx=10)
 
-MenuOpen = PhotoImage(file="menuOpen.png")
-MenuClose = PhotoImage(file="menuClose.png")
+button2 = ctk.CTkLabel(master=frame, text="Login2")
+button2.pack(pady=12, padx=10)
+
+checkbox = ctk.CTkCheckBox(master=frame, text="nas")
+checkbox.pack(pady=12, padx=10)
 
 root.mainloop()
