953ca33d4174000b21944879ad197d3c81e42eee
Edward Wang <115112114+Edward-W7@users.noreply.github.com>
2023-05-06 09:16:09 -0400

    Update searching.py

--- searching.py
+++ searching.py
@@ -10,7 +10,14 @@ def google_search(search_term, api_key, cse_id, **kwargs):
     res = service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()
     return res['items']
 
+def search_results(search_term):
+    try:
+        results = google_search(search_term, my_api_key, my_cse_id, num=3)
+    except KeyError:
+        results = None
 
-results = google_search('"god is a woman" "thank you next" "7 rings"', my_api_key, my_cse_id, num=10)
-for result in results:
-    pprint.pprint(result)
+#for result in results:
+    #print(result['title'])
+    #print(result['formattedUrl'] + '\n')
+
+    return results
