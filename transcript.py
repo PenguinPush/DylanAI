c7fa060d0817ce84aab83f1c1458cd92ef445dbc
PenguinPush <icyzminecraft@gmail.com>
2023-05-06 01:53:01 -0400

    fdsfsfds

--- transcript.py
+++ transcript.py
@@ -1,4 +1,7 @@
 import io
+
+import pyaudio
+import speech_recognition
 from pydub import AudioSegment
 import speech_recognition as sr
 import whisper
@@ -15,15 +18,10 @@ from scipy.io.wavfile import write
 
 openai.api_key = "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"
 
-@click.command()
-@click.option("--model", default="base", help="Model to use", type=click.Choice(["tiny","base", "small","medium","large"]))
-@click.option("--device", default=("cuda" if torch.cuda.is_available() else "cpu"), help="Device to use", type=click.Choice(["cpu","cuda"]))
-@click.option("--english", default=True, help="Whether to use English model",is_flag=True, type=bool)
-@click.option("--verbose", default=False, help="Whether to print verbose output", is_flag=True,type=bool)
-@click.option("--energy", default=1000, help="Energy level for mic to detect", type=int)
-@click.option("--dynamic_energy", default=True, is_flag=True, help="Flag to enable dynamic energy", type=bool)
-@click.option("--pause", default=0.3, help="Pause time before entry ends", type=float)
-@click.option("--save_file",default=False, help="Flag to save file", is_flag=True,type=bool)
+class Variables:
+    i = 0
+    filename = (f"temp{i}.wav")
+    wav_checked = False
 
 def main(model, english,verbose, energy, pause,dynamic_energy,save_file,device):
     #there are no english models for large
@@ -55,30 +53,38 @@ def record_audio(audio_queue, energy, pause, dynamic_energy, save_file, temp_dir
         print("Model Loaded!")
         i = 0
         while True:
-            audio = r.listen(source, phrase_time_limit=5, )
-            data = io.BytesIO(audio.get_wav_data())
-            filename = (f"temp{i}.wav")
-            audio_data = filename
-            write(filename, 8000, np.frombuffer(audio.frame_data))
+            audio = r.listen(source, phrase_time_limit=5)
+            data = np.frombuffer(audio.frame_data, np.int16).flatten().astype(np.float32) / 32768.0
+            Variables.filename = (f"temp{Variables.i}.wav")
+            audio_data = Variables.filename
+            write(Variables.filename, 16000, data)
             audio_queue.put_nowait(audio_data)
 
-            i += 1
-            wav_checked = False
+            Variables.i += 1
+            Variables.wav_checked = False
 
 def transcribe_forever(audio_queue, result_queue, audio_model, english, verbose, save_file):
-    wav_checked = True
+    Variables.wav_checked = True
     while True:
-        if os.path.exists(filename) and wav_checked == False:
-            wav_checked = True
-            audio_file = open(filename, "rb")
+        if os.path.exists(Variables.filename) and Variables.wav_checked == False:
+            Variables.wav_checked = True
+            audio_file = open(Variables.filename, "rb")
             result = openai.Audio.transcribe("whisper-1", audio_file)
             predicted_text = result["text"]
             result_queue.put_nowait(predicted_text)
 
 if __name__ == "__main__":
-    i = 0
     temp_dir = tempfile.mkdtemp()
-    filename = (f"temp{i}.wav")
     for tempfilename in glob.glob("./temp*"):
         os.remove(tempfilename)
-    main()
+
+    r = sr.Recognizer()
+    print("Pytorch CUDA Version is", torch.cuda.is_available())
+    with sr.Microphone() as source:
+        print("Please wait. Calibrating microphone...")
+        r.adjust_for_ambient_noise(source, duration=5)
+
+    transcript = open("transcript.txt", 'r+')
+    transcript.truncate(0)
+
+    main(model="base", device=("cuda" if torch.cuda.is_available() else "cpu"), english=True, verbose=False, energy=1000, dynamic_energy=True, pause=0.3, save_file=False)
