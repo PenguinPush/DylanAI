1bb0f423493d44421ba1ee8656b55f386e5f0eab
Edward Wang <115112114+Edward-W7@users.noreply.github.com>
2023-05-07 04:43:47 -0400

    Update macros.py

--- macros.py
+++ macros.py
@@ -7,94 +7,78 @@ import webbrowser
 import subprocess
 confidence_threshold = 0.8
 
+
 import cohere
+
 co = cohere.Client('a3q94Odywjq3jBIDEdFlvFDVXeDDhTTOJ9g56WY9')
+
+
 class VariablesMacros:
     valid = 0
     command = ""
 
-def read_info(key, item, text):
 
+def read_info(key, item, text):
     match key:
         case "validity":
             if item[1] > confidence_threshold / 4:
-               match item[0]:
-                   case "valid command":
-                       VariablesMacros.valid = 1
-                   case "not computer related":
-                       VariablesMacros.valid = 2
-                   case "invalid command":
-                       VariablesMacros.valid = 0
-
+                match item[0]:
+                    case "valid command":
+                        VariablesMacros.valid = 1
+                    case "not computer related":
+                        VariablesMacros.valid = 2
+                    case "invalid command":
+                        VariablesMacros.valid = 0
 
         case "command":
             if item[1] > confidence_threshold:
                 VariablesMacros.command = item[0]
 
         case "subject":
-            if item[1] > confidence_threshold:
-                item_location = default_list_dict.get(item[0])
-                print(item_location)
+            item_location = default_list_dict.get(item[0])
+            print(item_location)
 
-                if item_location.startswith("http"):
-                    subject_type = "url"
-                else:
-                    subject_type = "path"
-
-                if VariablesMacros.valid == 1 and VariablesMacros.command == "open":
-                    if subject_type == "path":
-                        subprocess.Popen(item_location)
-                        VariablesMacros.valid = False
-                        VariablesMacros.command = ""
-
-                    if subject_type == "url":
-                        webbrowser.open(item_location)
-                        VariablesMacros.valid = False
-                        VariablesMacros.command = ""
-
-                elif VariablesMacros.valid == 1 and VariablesMacros.command == "search":
-                    if VariablesMacros.valid != 0:
-                        search_term = get_searchable_term(text)
-                        print(f"the searched term is: {search_term}")
-                        results = search_results(search_term, 3)
-                        if results:
-                            for result in results:
-                                if not result:
-                                    break
-                                else:
-                                    print(result['title'])
-                                    print(result['formattedUrl'] + '\n')
-                            webbrowser.open(results[0]['link'])
-                            VariablesMacros.valid = False
-                            VariablesMacros.command = ""
-
-                elif VariablesMacros.valid == 1 and VariablesMacros.command == "type":
-                    if VariablesMacros.valid != 0:
-                        type_term = get_typeable_term(text)
-                        print(f"the typed term is: {type_term}")
-                        keyboard.write(type_term)
-                elif VariablesMacros.valid != 0:
-                    response = co.chat(
-                        query=text,
-                        temperature=0.7,
-                    )
-                    print(response.text)
-                    return response.text
-
+            if item_location.startswith("http"):
+                subject_type = "url"
             else:
+                subject_type = "path"
+
+            if VariablesMacros.valid == 1 and VariablesMacros.command == "open":
+                if subject_type == "path":
+                    subprocess.Popen(item_location)
+                    VariablesMacros.valid = False
+                    VariablesMacros.command = ""
+
+                if subject_type == "url":
+                    webbrowser.open(item_location)
+                    VariablesMacros.valid = False
+                    VariablesMacros.command = ""
+
+            elif VariablesMacros.valid == 1 and VariablesMacros.command == "search":
+                search_term = get_searchable_term(text)
+                print(f"the searched term is: {search_term}")
+                results = search_results(search_term, 3)
+                if results:
+                    for result in results:
+                        if not result:
+                            break
+                        else:
+                            print(result['title'])
+                            print(result['formattedUrl'] + '\n')
+                    webbrowser.open(results[0]['link'])
+                    VariablesMacros.valid = False
+                    VariablesMacros.command = ""
+
+            elif VariablesMacros.valid == 1 and VariablesMacros.command == "type":
                 if VariablesMacros.valid != 0:
-
-                    response = co.chat(
-                        query=text,
-                        temperature=0.7,
-                    )
-                    print(response.text)
-                    return response.text
-
-#inpt = input()
-#categories = categorize(inpt)
-#for key, value in categories.items():
-#    read_info(key, value, inpt)
-#    print(f"{key}: {value}")
-
+                    type_term = get_typeable_term(text)
+                    print(f"the typed term is: {type_term}")
+                    keyboard.write(type_term)
+            elif VariablesMacros.valid != 0:
+                response = co.chat(
+                    query=text,
+                    temperature=0.7,
+                )
+                print(response.text)
+                return response.text
 
